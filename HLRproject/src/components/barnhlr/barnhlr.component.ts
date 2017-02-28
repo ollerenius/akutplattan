d3422735e535d3a1650e48b1673187ef4891f465
Daniel Månsson <danma344@student.liu.se>
2017-02-28 12:27:07 +0100

    Added template for barn-hlr page.

---
+++ HLRproject/src/components/barnhlr/barnhlr.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'barnhlr-page',
+  templateUrl: './barnhlr.component.html',
+  styleUrls: ['./barnhlr.component.css']
+})
+export class BarnHLRComponent {
+  title = 'BarnHLR component';
+}
