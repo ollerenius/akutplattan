9a458c3381bd63a561dd101962e10a0fe079139d
Daniel Månsson <danma344@student.liu.se>
2017-02-28 12:37:57 +0100

    Added hlr page template and hlr-flow (for pages) template.

---
+++ HLRproject/src/components/hlr/hlr.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hlrflow-page',
+  templateUrl: './hlr.component.html',
+  styleUrls: ['./hlr.component.css']
+})
+export class HLRComponent {
+  title = 'HLR page';
+}
---
+++ HLRproject/src/components/hlrflow/hlrflow.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hlrflow-page',
+  templateUrl: './hlrflow.component.html',
+  styleUrls: ['./hlrflow.component.css']
+})
+export class HLRFlowComponent {
+  title = 'HLR-flow component';
+}
