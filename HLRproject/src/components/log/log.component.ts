30819e223921b0bda1cb32ef1410e5cee1025c49
Daniel Månsson <danma344@student.liu.se>
2017-02-28 12:13:23 +0100

    Added log-page-component template.

---
+++ HLRproject/src/components/log/log.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'log-page',
+  templateUrl: './log.component.html',
+  styleUrls: ['./log.component.css']
+})
+export class AppComponent {
+  title = 'Log app page';
+}
