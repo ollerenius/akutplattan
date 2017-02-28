6f5618021f9417f04d2a46ec86142c3916df3120
Daniel Månsson <danma344@student.liu.se>
2017-02-28 12:18:51 +0100

    Fixed naming of mainmenu component.

--- HLRproject/src/components/mainmenu/mainmenu.component.ts
+++ HLRproject/src/components/mainmenu/mainmenu.component.ts
@@ -4,7 +4,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'mainmenu-page',
   templateUrl: './mainmenu.component.html',
   styleUrls: ['./mainmenu.component.css']
 })
