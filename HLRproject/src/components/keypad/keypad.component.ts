18d967d960e57393d12e3fc48adbbb6b72eed231
Oskar Joelsson <oskjo581@student.liu.se>
2017-03-21 08:35:20 +0100

    Started building keypad prototype, using bootstrap std buttons. TypeScript code is messy testing, with string and number. Will be fixed today.

--- HLRproject/src/components/keypad/keypad.component.ts
+++ HLRproject/src/components/keypad/keypad.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class KeyPadComponent {
   title = 'Keypad component';
+
+
+
+  firstTime: boolean = true;
+  weight: number = 0;
+  testList: Array<number> = [];
+
+  public setManualWeight(pressedButton){
+    let outputText: string = 'Enter Weight';
+
+    if(pressedButton == 'OK')
+    {
+      this.testList.push(this.weight);
+      this.weight = 0;
+      this.title = outputText;
+      this.firstTime = true;
+    }
+    else if(pressedButton == 'C')
+    {
+      this.title = outputText;
+      this.firstTime = true;
+    }
+    else if (this.firstTime)
+    {
+      this.weight = 10 * this.weight + pressedButton;
+      this.title = String(pressedButton);
+      this.firstTime = false;
+    }
+    else
+    {
+      this.weight = 10 * this.weight + pressedButton;
+      this.title += String(pressedButton);
+    }
+  }
 }
