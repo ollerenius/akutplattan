8ba44425bc520ccacbc447347fc855c98d30531b
Oskar Joelsson <oskjo581@student.liu.se>
2017-03-21 08:52:33 +0100

    Fixed the dependency problem, was importing not using the dependency for component. Started building keypad prototype, using bootstrap std buttons. TypeScript code is messy testing, with string and number. Will be fixed today.

--- HLRproject/src/app/app.module.ts
+++ HLRproject/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+
 import { AppComponent } from './app.component';
+import {KeyPadComponent} from "../components/keypad/keypad.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KeyPadComponent
   ],
   imports: [
     BrowserModule,
