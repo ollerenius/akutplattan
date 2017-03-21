b2e36cf224d1528e44188822e2fa7eb4bd0784ae
Daniel Månsson <danma344@student.liu.se>
2017-03-21 11:12:36 +0100

    Created new barnhlr_start component template.

---
+++ HLRproject/src/components/barnhlr_start/barnhlr_start.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'barnhlr-startpage',
+  templateUrl: 'barnhlr_start.component.html',
+  styleUrls: ['barnhlr_start.component.css']
+})
+export class BarnHLRStartComponent {
+  title = 'BarnHLR start';
+  vikt = 0;
+}
