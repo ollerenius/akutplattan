18deef94f36e9a10726e1259b198e96e6125e57b
Olle Renius <ollerenius@Olles-MacBook-Pro.local>
2017-03-22 20:58:47 +0100

    Add a functional clock that counts hours, minutes and seconds

--- HLRproject/src/components/header/header.component.ts
+++ HLRproject/src/components/header/header.component.ts
@@ -11,31 +11,56 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent {
-  title = 'Titel';
 
+  title = 'Titel';
   leftArrow: string;
+  currentTime : string;
 
   constructor(){
-    this.leftArrow = '../assets/images/left-arrow.jpg'
+    this.leftArrow = '../assets/images/left-arrow.jpg';
+    this.startClock();
   }
-
-  currentTime : string = this.getCurrentTime();
-  private getCurrentTime() : string {
-    var date : Date = new Date();
-    var hours : number = date.getHours();
-    var minutes : number = date.getMinutes();
-    var seconds : number = date.getMinutes();
-    var currentTime : string = hours + ':' + minutes + ':' + seconds;
-    return currentTime;
-  }
-
-  updateTime() : void {
-    this.currentTime = this.getCurrentTime();
-  }
-
 
   goBack() : void {
     console.log('You pressed the Back-button.');
+  }
+
+  /**
+   * Use this method to set the current time and start the clock.
+   * setInterval() will call updateClock once every second.
+   */
+  private startClock() : void {
+    this.updateClock();
+    setInterval(() => {
+      this.updateClock();
+    }, 1000);
+  }
+
+  /**
+   * This method is called to get the current time.
+   * That value is formatted properly and assigned to currentTime.
+   */
+  private updateClock() : void {
+    // Get time values
+    let date : Date = new Date();
+    let hours : number = date.getHours();
+    let minutes : number = date.getMinutes();
+    let seconds : number = date.getSeconds();
+    // Store the same values as strings
+    let hoursString : string = String(hours);
+    let minutesString : string = String(minutes);
+    let secondsString : string = String(seconds);
+    // Format strings properly
+    if (hours < 10) {
+      hoursString = '0' + hours;
+    }
+    if (minutes < 10) {
+      minutesString = '0' + minutes;
+    }
+    if (seconds < 10) {
+      secondsString = '0' + seconds;
+    }
+    this.currentTime = hoursString + ':' + minutesString + ':' + secondsString;
   }
 
 }
