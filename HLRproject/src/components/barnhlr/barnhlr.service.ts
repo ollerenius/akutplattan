128e1a09bf93d8b59790ca1cf13192dcb07e457f
Daniel Månsson <danma344@student.liu.se>
2017-03-23 13:22:22 +0100

    Cleanup before merge from main.

--- HLRproject/src/components/barnhlr/barnhlr.service.ts
+++ HLRproject/src/components/barnhlr/barnhlr.service.ts
@@ -1,8 +1,5 @@
 import { Injectable }  from '@angular/core';
 
-import { BarnHLRStartComponent } from './../barnhlr/barnhlr.component';
-import { BarnHLRSettingsComponent } from './../barnhlrsettings/barnhlrsettings.component';
-
 @Injectable()
 export class BarnHLRService{
   getSettings(): boolean {
