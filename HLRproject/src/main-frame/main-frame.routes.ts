b49defb9a0e48a85ae94dd9c5f55aa676a065f28
Daniel Månsson <danma344@student.liu.se>
2017-03-23 13:50:47 +0100

    Implemented routing for barnhlr.

--- HLRproject/src/main-frame/main-frame.routes.ts
+++ HLRproject/src/main-frame/main-frame.routes.ts
@@ -1,9 +1,13 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainMenuComponent } from '../components/mainmenu/mainmenu.component';
+import {BarnHLRStartComponent} from "../components/barnhlr/barnhlr.component";
+import {BarnHLRSettingsComponent} from "../components/barnhlrsettings/barnhlrsettings.component";
 
 export const routes: Routes = [
   { path: '', component: MainMenuComponent },
+  { path: 'barnhlr', component: BarnHLRStartComponent},
+  { path: 'barnhlr/calc', component: BarnHLRSettingsComponent}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
