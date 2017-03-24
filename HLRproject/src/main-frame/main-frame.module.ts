9422db6903077799b6967e41610f78e3f7d21336
Kim <kimla207@student.liu.se>
2017-03-24 14:23:22 +0100

    With this commit the functionality of the header is implemented and navigation from det main many to HLR can be done. Functionality on the HLR-Flow is to be implemented.

--- HLRproject/src/main-frame/main-frame.module.ts
+++ HLRproject/src/main-frame/main-frame.module.ts
@@ -3,22 +3,36 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+
 import { MainFrameComponent } from './main-frame.component';
 import { MainMenuComponent } from '../components/mainmenu/mainmenu.component';
-import {HeaderComponent} from "../components/header/header.component";
+import { HeaderComponent } from "../components/header/header.component";
+import { HLRComponent } from "../components/hlr/hlr.component";
 import {routing} from "./main-frame.routes";
+import { ChecklistComponent } from "../components/hlr/checklist/checklist.component";
+import { HLRFlowComponent } from "../components/hlr/hlrflow/hlrflow.component";
+import { PopoverAnalysisComponent } from "../components/hlr/popover_analysis/popover_analysis.component";
+import { PopoverMedicineComponent } from "../components/hlr/popover_medicine/popover_medicine.component";
+import { PopoverModule, ButtonsModule } from "ng2-bootstrap";
 
 @NgModule({
   declarations: [
     MainFrameComponent,
     MainMenuComponent,
-    HeaderComponent
+    HeaderComponent,
+    HLRComponent,
+    ChecklistComponent,
+    HLRFlowComponent,
+    PopoverAnalysisComponent,
+    PopoverMedicineComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
+    routing,
+    PopoverModule.forRoot(),
+    ButtonsModule.forRoot()
   ],
   providers: [],
   bootstrap: [MainFrameComponent]
