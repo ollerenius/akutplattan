d8305f1c96d3f9c15c09bcdf0086d1befd09aee4
lisax909 <lisax909@student.liu.se>
2017-03-27 15:48:18 +0200

    This commit changes the file structure in hlr folder

--- HLRproject/src/components/hlr/hlr.module.ts
+++ HLRproject/src/components/hlr/hlr.module.ts
@@ -2,28 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { PopoverModule } from 'ng2-bootstrap/popover';
-import { ButtonsModule } from 'ng2-bootstrap/buttons';
 
 import {HLRComponent} from "./hlr.component";
+
 import {ChecklistComponent } from "./checklist/checklist.component"
 import {HLRFlowComponent} from "./hlrflow/hlrflow.component";
-import { PopoverAnalysisComponent } from './popover_analysis/popover_analysis.component';
-import { PopoverMedicineComponent } from './popover_medicine/popover_medicine.component';
-import {HeartmassagetextComponent} from './heartmassagetext/heartmassagetext.component'
 
 @NgModule({
   declarations: [
     HLRComponent,
     ChecklistComponent,
-    PopoverAnalysisComponent,
-    PopoverMedicineComponent,
-    HLRFlowComponent,
-    HeartmassagetextComponent
+    HLRFlowComponent
   ],
   imports: [
-    PopoverModule.forRoot(),
-    ButtonsModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule
--- HLRproject/src/components/hlr/hlrflow/hlrflow.module.ts
+++ HLRproject/src/components/hlr/hlrflow/hlrflow.module.ts
@@ -1,28 +1,31 @@
-/**
- * Created by Kim on 2017-03-21.
- */
-
-// These module is not used in the current state since we do not import
-// anything into the component at the moment. This module is just created
-// in case we want to import something later.
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { PopoverModule } from 'ng2-bootstrap/popover';
+import { ButtonsModule } from 'ng2-bootstrap/buttons';
 
 import {HLRFlowComponent} from "./hlrflow.component";
 
+import { PopoverAnalysisComponent } from './popover_analysis/popover_analysis.component';
+import { PopoverMedicineComponent } from './popover_medicine/popover_medicine.component';
+import {HeartmassagetextComponent} from './heartmassagetext/heartmassagetext.component'
+
 @NgModule({
   declarations: [
-    HLRFlowComponent
+    HLRFlowComponent,
+    PopoverAnalysisComponent,
+    PopoverMedicineComponent,
+    HeartmassagetextComponent
   ],
   imports: [
+    PopoverModule.forRoot(),
+    ButtonsModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule
   ],
   providers: [],
-  bootstrap: [HLRFlowComponent]
+  bootstrap: [HLRFlowComponent, PopoverAnalysisComponent, PopoverMedicineComponent, HeartmassagetextComponent]
 })
 export class HLRFlowModule { }
