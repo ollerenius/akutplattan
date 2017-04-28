401642a86cdaeaa0f9f65e0cc51a733649b54337
lisax909 <lisax909@student.liu.se>
2017-04-28 15:02:07 +0200

    Correctly calculate the adrenaline & amiodarone dose in barn-hlr

--- HLRproject/src/components/barnhlrsettings/barnhlrsettings.component.ts
+++ HLRproject/src/components/barnhlrsettings/barnhlrsettings.component.ts
@@ -17,8 +17,7 @@ export class BarnHLRSettingsComponent{
   useAge: boolean;
   wetflag : string = "Ogiltig";
   inputRadioModel : string = "Ar";
-  calcUnit : string;
-  oppositeCalcUnit : string;
+  newCalcUnit : string;
   private lastKeypadString : string = "";
 
   onNotify(keypadString : string) : void{
@@ -30,16 +29,11 @@ export class BarnHLRSettingsComponent{
   constructor(private barnHLRService: BarnHLRService, private hlrDosageService : HLRDosageService) {
     this.useAge = barnHLRService.bool_val;
 
-    /**
-     *
-     */
     if (this.useAge == true){
-      this.calcUnit = "ålder";
-      this.oppositeCalcUnit = "vikt";
+      this.newCalcUnit = "vikt";
     }
     else{
-      this.calcUnit = "vikt";
-      this.oppositeCalcUnit = "ålder";
+      this.newCalcUnit = "ålder";
     }
   }
 
@@ -50,13 +44,11 @@ export class BarnHLRSettingsComponent{
   switchKeypad(currentState : boolean){
     if (currentState == true){
       this.useAge = false;
-      this.calcUnit =  "vikt";
-      this.oppositeCalcUnit = "ålder";
+      this.newCalcUnit = "ålder";
     }
     else{
       this.useAge = true;
-      this.calcUnit = "ålder";
-      this.oppositeCalcUnit = "vikt";
+      this.newCalcUnit = "vikt";
     }
   }
 
@@ -66,109 +58,108 @@ export class BarnHLRSettingsComponent{
   private updateDisplayedWetflagValue(){
     let inputType = this.inputRadioModel;
     if(this.verifyStringFromKeypad(this.lastKeypadString, inputType)) {
+      let wetflagNumber: number = this._wetflagTransform(this.lastKeypadString, inputType);
       if (this.useAge == false) {
         this.wetflag = String(this.lastKeypadString + " kg");
       }
-      else {
-        let wetflagNumber: number = this._wetflagTransform(this.lastKeypadString, inputType);
+      else{
         this.wetflag = String(wetflagNumber + " kg");
-        //TODO: Only navigate to HLR after we have a real weight (not on undefined etc..)
-        //TODO: Fix it so that this part guarantees safety (eg. that this.wetflag is good number)
-        this.hlrDosageService.setDosagesFromWeight(Number(wetflagNumber));
       }
+      //TODO: Only navigate to HLR after we have a real weight (not on undefined etc..)
+      //TODO: Fix it so that this part guarantees safety (eg. that this.wetflag is good number)
+      this.hlrDosageService.setDosagesFromWeight(Number(wetflagNumber));
     }
     else{
       this.wetflag = "Ogiltig";
     }
   }
 
-  /**
-   * This function verifies that the input holds to the expected input type.
-   * @param keypadString : string The input string to verify.
-   * @param expectedInputType : string The expected input type.
-   * @returns {boolean} True if input holds to the type specification, false if not.
-   */
-  private verifyStringFromKeypad(keypadString : string, expectedInputType : string) : boolean{
-    switch(expectedInputType){
-      case "Ar": case "Manader":
-        return !isNaN(Number(keypadString));
-      case "Personnummer":
-        return (keypadString.length == 8 || keypadString.length == 12)  && !isNaN(Number(keypadString));
-      default:
-        console.error("Unsupported input format type expected. Please check the radiomodel for clues to this error. (type="+expectedInputType+")");
-        return false;
-    }
+/**
+ * This function verifies that the input holds to the expected input type.
+ * @param keypadString : string The input string to verify.
+ * @param expectedInputType : string The expected input type.
+ * @returns {boolean} True if input holds to the type specification, false if not.
+ */
+private verifyStringFromKeypad(keypadString : string, expectedInputType : string) : boolean{
+  switch(expectedInputType){
+    case "Ar": case "Manader":
+    return !isNaN(Number(keypadString));
+    case "Personnummer":
+      return (keypadString.length == 8 || keypadString.length == 12)  && !isNaN(Number(keypadString));
+    default:
+      console.error("Unsupported input format type expected. Please check the radiomodel for clues to this error. (type="+expectedInputType+")");
+      return false;
+  }
+}
+
+/**
+ * Calculates the age of a (swedish) person number in months and returns it.
+ * !!! REQUIRES EXTERNAL SANITY CHECK !!! (see this.verifyStringFromKeypad)
+ * Pnumber is YYYYMMDD or YYYYMMDDxxxx so len(pn) == 8 | 12
+ * @param pnumber A person number (last 4 digits optional and the last 6 will be ignored)
+ * @returns {number} The age of the person, rounded to closest age in months.
+ */
+private pnToMonths(pnumber : string) : number{
+  //TODO: This does NOT count days, what can be done is dDays - pnDays where 15 is round to 1 and lower 0 (in added months)
+  //TODO: Better sanity check (pnYears > dYears is possible right now.. as well as pnMonths > 12)
+  //TODO: Change format to YYMMDD / YYMMDDxxxx instead. This requires sanity check in form of rounding to the best full years (eg. not 19xx when xx=07 but not 20xx when xx=99)
+  //Get current year, month and day.
+  let date : Date = new Date();
+  let dYears : number = date.getFullYear();
+  let dMonths : number = date.getMonth() + 1; //Otherwise 0 -> 11
+
+  //Still, only the first 6 numbers is interesting.
+  let pnYears : number = Number(pnumber.slice(0,4));
+  let pnMonths : number = Number(pnumber.slice(4,6));
+
+  return (dYears - pnYears) * 12 + (dMonths - pnMonths);
+}
+
+/**
+ * Takes an age formatted in a chosen way and returns the estimated weight for that age.
+ * @param ageStr : string The input string we want to calculate weight for.
+ * @param inputType : string The input type string for which the age is formatted in.
+ * @returns {number} The estimated weight.
+ */
+private _wetflagTransform(ageStr : string, inputType : string) : number{
+  let months : number = 0;
+  let years : number = 0;
+
+  switch(inputType){
+    case "Personnummer":
+      months = this.pnToMonths(ageStr);
+      years = Math.floor(months/12);
+      months -= years * 12;
+      break;
+    case "Ar":
+      months = 0;
+      years = Number(ageStr);
+      break;
+    case "Manader":
+      months = Number(ageStr);
+      years = Math.floor(months/12);
+      months -= years * 12;
+      break;
+    default:
+      console.error("Undefined input radio model picked.")
+      //TODO: If we get here we have some real trouble ahead of us. Can we get here?
+      break;
   }
 
-  /**
-   * Calculates the age of a (swedish) person number in months and returns it.
-   * !!! REQUIRES EXTERNAL SANITY CHECK !!! (see this.verifyStringFromKeypad)
-   * Pnumber is YYYYMMDD or YYYYMMDDxxxx so len(pn) == 8 | 12
-   * @param pnumber A person number (last 4 digits optional and the last 6 will be ignored)
-   * @returns {number} The age of the person, rounded to closest age in months.
-   */
-  private pnToMonths(pnumber : string) : number{
-    //TODO: This does NOT count days, what can be done is dDays - pnDays where 15 is round to 1 and lower 0 (in added months)
-    //TODO: Better sanity check (pnYears > dYears is possible right now.. as well as pnMonths > 12)
-    //TODO: Change format to YYMMDD / YYMMDDxxxx instead. This requires sanity check in form of rounding to the best full years (eg. not 19xx when xx=07 but not 20xx when xx=99)
-    //Get current year, month and day.
-    let date : Date = new Date();
-    let dYears : number = date.getFullYear();
-    let dMonths : number = date.getMonth() + 1; //Otherwise 0 -> 11
+  let outputWetflag : number;
 
-    //Still, only the first 6 numbers is interesting.
-    let pnYears : number = Number(pnumber.slice(0,4));
-    let pnMonths : number = Number(pnumber.slice(4,6));
-
-    return (dYears - pnYears) * 12 + (dMonths - pnMonths);
+  if(years >= 13){
+    outputWetflag = 50;
   }
-
-  /**
-   * Takes an age formatted in a chosen way and returns the estimated weight for that age.
-   * @param ageStr : string The input string we want to calculate weight for.
-   * @param inputType : string The input type string for which the age is formatted in.
-   * @returns {number} The estimated weight.
-   */
-  private _wetflagTransform(ageStr : string, inputType : string) : number{
-    let months : number = 0;
-    let years : number = 0;
-    //TODO: Implement functionality for years / months instead of only months
-    switch(inputType){
-      case "Personnummer":
-        months = this.pnToMonths(ageStr);
-        years = Math.floor(months/12);
-        months -= years * 12;
-        break;
-      case "Ar":
-        months = 0;
-        years = Number(ageStr);
-        break;
-      case "Manader":
-        months = Number(ageStr);
-        years = Math.floor(months/12);
-        months -= years * 12;
-        break;
-      default:
-        console.error("Undefined input radio model picked.")
-        //TODO: If we get here we have some real trouble ahead of us. Can we get here?
-        break;
-    }
-
-    let outputWetflag : number;
-
-    if(years > 13){
-      //TODO: Is this really the upper cap? This should be used to limit the output value.
-      outputWetflag = 43;
-    }
-    else if(years > 5 && years < 13){
-      outputWetflag = 3*years + 7;
-    }
-    else if(years < 6 && years > 0){
-      outputWetflag = 2*years + 8;
-    }
-    else if(years == 0){
-      outputWetflag = 0.5 * months + 4;
-    }
-    return outputWetflag;
+  else if(years >= 6 && years < 13){
+    outputWetflag = 3*years + 7;
   }
+  else if(years < 6 && years > 0){
+    outputWetflag = 2*years + 8;
+  }
+  else if(years == 0){
+    outputWetflag = 0.5 * months + 4;
+  }
+  return outputWetflag;
+}
 }
--- HLRproject/src/services/hlrdosage.service.ts
+++ HLRproject/src/services/hlrdosage.service.ts
@@ -9,24 +9,31 @@ import {Injectable} from "@angular/core";
 @Injectable()
 export class HLRDosageService {
   /**
-   * The current time as a string since when the last HLR-flow was initiated.
+   * Standard dose for an adult
    */
-  amiodarone : number = 20;
   adrenaline : number = 10;
-
-  public setDosagesFromWeight(weightKg : number) : void{
-    //TODO: Make something useful here
-    this.adrenaline = weightKg * 0.1;
-    this.amiodarone = weightKg * 0.33;
+  amiodarone : number = 20;
 
 
-    /*
-     From table:
-     Adrenaline (0.1 mg/ml) 0.01 mg/kg, 0.1 ml/kg
-     Amiodarone 15mg/ml* 5 mg/kg, 0.33 ml/kg
+  /**
+   * Sets the doses for a person based on the persons weight
+   * @param weightKg :number - the persons weight in kg
+   */
+  public setDosagesFromWeight(weightKg : number) : void {
+    if (weightKg < 50) {
+      this.adrenaline = Math.round(weightKg * 0.1);
+      this.amiodarone = Math.round(weightKg * 0.33);
+      /*
+       From table:
+       Adrenaline (0.1 mg/ml) 0.01 mg/kg, 0.1 ml/kg
+       Amiodarone 15mg/ml* 5 mg/kg, 0.33 ml/kg
 
-     *Amiodarone 50mg/ml. 6 ml spädes med 14 ml glukos 50mg/ml = 15 mg/ml
-
-     */
+       *Amiodarone 50mg/ml. 6 ml spädes med 14 ml glukos 50mg/ml = 15 mg/ml
+       */
+    }
+    else{
+      this.adrenaline = 10;
+      this.amiodarone = 20;
+    }
   }
 }
