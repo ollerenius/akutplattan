90ee88750b4c16eeccea24db1ba8160add0cb41b
Oscar Magnusson <balobba@gmail.com>
2017-05-01 18:03:41 +0200

    Fixes to respiratory page.
    Added cropped image to respiratory page. Added settings to adjust image to (any) device's screen (preventing from scrolling horizontally). Renamed image variables.

--- HLRproject/src/components/respiratoryarrest/respiratoryarrest.component.ts
+++ HLRproject/src/components/respiratoryarrest/respiratoryarrest.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-respiratory-arrest',
+  selector: 'respiratory-arrest',
   templateUrl: './respiratoryarrest.component.html',
   styleUrls: ['./respiratoryarrest.component.css']
 })
 export class RespiratoryArrestComponent{
 //TODO: Replace this current image with a cropped one that fits an ipad screen without scrolling.
-  REPLACE_THIS_IMAGE_WITH_A_CROPPED_ONE: string = '../../../assets/images/respiratoryarrest.png';
+  respiratory_arrest_instruction_image: string = '../../../assets/images/respiratoryarrest.png';
 
 }
